b78fdcf417f52124bc0985cb6fd5f3878136153e
Anh Hào Phan <pdkenny25@gmail.com>
2025-10-15 13:21:59 +0700

    #22. Sử dụng Variables với JSX

--- src/test/second.component.tsx
+++ src/test/second.component.tsx
@@ -1,10 +1,28 @@
 
 const SecondComponent = () => {
 
+    const name = "PhAHao";
+    const age = 22;
+
+    const info = {
+        address: "VN",
+        regionId: 75
+    };
+    const arr = [1, 2, 3, true, { sound: "hehehe" }];
+    const testBoolean = true; // boolean => render nothing
+    const testNull = null; // null => render nothing
+    const testUndifine = undefined; // undefined => render nothing
+
     // jsx: html + js => 1 block - là cú pháp viết trong hàm return này
     return (
         <div>
-            <h1>Hedy Lamarr's Todos</h1>
+            <h1 style={
+                {
+                    borderRadius: "5px",
+                    border: "1px solid green",
+                    color: "red"
+                }
+            }>Hedy Lamarr's Todos</h1>
             <img
                 src="https://i.imgur.com/yXOvdOSs.jpg"
                 alt="Hedy Lamarr"
@@ -15,7 +33,8 @@ const SecondComponent = () => {
                 <li>Rehearse a movie scene </li>
                 <li>Improve the spectrum technology</li>
             </ul>
-
+            <h5>{JSON.stringify(arr)}</h5>
+            <footer>Make by {name}, age {age}, {JSON.stringify(info)}</footer>
         </div>
     );
 };
