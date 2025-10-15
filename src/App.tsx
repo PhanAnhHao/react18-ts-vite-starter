0aa75d545b42dd34498e32336a0732e09c3d4128
Anh Hào Phan <pdkenny25@gmail.com>
2025-10-15 13:32:53 +0700

    #23. Bài tập design Component Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import MyFirstComponent from './test/my.component';
-import SecondComponent from './test/second.component';
+// import './App.css';
+import InputTodo from './todo/input.todo';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-
-        {/* ko khuyến kích viết mở tag và đóng tag -> dài dòng */}
-        {/* <MyFirstComponent></MyFirstComponent> */}
-
-        {/* cách viết ngắn gọn hơn */}
-        <MyFirstComponent />
-        <SecondComponent />
-
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <InputTodo />
       </div>
-      <h1>Vite + React</h1>
-      <h2>Hello world</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/todo/input.todo.jsx
@@ -0,0 +1,13 @@
+
+const InputTodo = () => {
+    return (
+        <div>
+            <div>Add new todo</div>
+            <input type="text" />
+            &nbsp; &nbsp; {/* HTML Entities - Non-breaking Space */}
+            <button>Save</button>
+        </div>
+    )
+}
+
+export default InputTodo;
