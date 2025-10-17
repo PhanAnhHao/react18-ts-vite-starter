b5d91ee2017468ba3573200da336e1b492d90efd
Anh Hào Phan <pdkenny25@gmail.com>
2025-10-17 19:33:42 +0700

    #37. Vòng đời của component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import InputTodo from './todo/input.todo';
 
 function App() {
 
+  const [count, setCount] = useState<number>(1);
   const name = "ChillFeel";
   const age = 22;
   const info = {
@@ -15,8 +16,17 @@ function App() {
     ["todo1", "todo2", "todo3", "todo4", "todo5", "todo6"]
   );
 
+  /**
+   *  Phase 1: Mounting (born) : được sinh ra -> chèn html vào giao diện (cây DOM)
+      Phase 2: Update: cập nhật giao diện sau khi đã có cây DOM
+      Phase 3: Unmounting (die): xóa html khỏi cây DOM
+
+   */
+
   return (
     <>
+      <div>count = {count}</div>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
       <div className='parent' id='taolabomay'>
         <div className="children" id='maylacontao'></div>
       </div>
