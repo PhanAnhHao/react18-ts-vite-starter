d7d6e526cb322d3eeccae1660c0ffb599a68b507
Anh Hào Phan <pdkenny25@gmail.com>
2025-10-18 12:38:55 +0700

    #50. Gọi API với React

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,6 +1,49 @@
+import { useEffect } from 'react';
 import '../../styles/users.css';
 
 const UsersTable = () => {
+
+    // update
+    useEffect(() => {
+        // viết api trong hàm useEffect, bởi vì hàm useEffect, chạy sau khi component đc mounting/render/cây DOM đã sẵn sàng rồi
+        console.log(">>> check useEffect");
+        getData();
+    }, []);  //tất cả những logic nào muốn update cho component thì viết trong useEffect
+
+    const getData = async () => {
+        const res = await fetch(
+            "http://localhost:8000/api/v1/auth/login",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    username: "",
+                    password: ""
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }); // method mặc định của fetch là GET
+        const data = await res.json();
+
+        const access_token = ""
+
+        console.log(">>> Check data: ", data);
+
+        const res1 = await fetch(
+            "http://localhost:8000/api/v1/users/all",
+            {
+                headers: {
+                    'Authorization': `Bearer ${access_token}`,
+                    "Content-Type": "application/json",
+                },
+            });
+
+        const data1 = await res1.json();
+        console.log(">>> Check data1: ", data1);
+    }
+
+    console.log(">>> check render"); //mounting
+
     return (
         <div>
             <h2>HTML Table</h2>
