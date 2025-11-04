e12522f4d7142826c3def19f8f514545d6fc9273
Anh Hào Phan <pdkenny25@gmail.com>
2025-11-04 09:41:44 +0700

    #72. Bài tập Paginate User

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -26,6 +26,12 @@ const UsersTable = () => {
     const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
 
     const [dataUpdate, setDataUpdate] = useState<null | IUser>(null);
+    const [meta, setMeta] = useState({
+        current: 1,
+        pageSize: 3,
+        pages: 0,
+        total: 0
+    });
 
     const access_token = localStorage.getItem("access_token") as string; // "as string" - ép kiểu dữ liệu
 
@@ -37,7 +43,7 @@ const UsersTable = () => {
     const getData = async () => {
 
         const res = await fetch(
-            "http://localhost:8000/api/v1/users/all",
+            `http://localhost:8000/api/v1/users?current=${meta.current}&pageSize=${meta.pageSize}`,
             {
                 headers: {
                     'Authorization': `Bearer ${access_token}`,
@@ -46,14 +52,19 @@ const UsersTable = () => {
             });
 
         const d = await res.json();
-        if (d && d?.data) {
-            setListUsers(d.data.result);
-        } else {
+        if (!d?.data) {
             notification.error({
                 message: JSON.stringify(d.message)
             });
-            setListUsers([]);
+            return;
         }
+        setListUsers(d.data.result);
+        setMeta({
+            current: d.data.meta.current,
+            pageSize: d.data.meta.pageSize,
+            pages: d.data.meta.pages,
+            total: d.data.meta.total
+        });
 
     };
 
@@ -156,6 +167,32 @@ const UsersTable = () => {
         },
     ];
 
+    const handleOnChange = async (page: number, pageSize: number) => {
+        const res = await fetch(
+            `http://localhost:8000/api/v1/users?current=${page}&pageSize=${pageSize}`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${access_token}`,
+                    "Content-Type": "application/json",
+                },
+            });
+
+        const d = await res.json();
+        if (!d?.data) {
+            notification.error({
+                message: JSON.stringify(d.message)
+            });
+            return;
+        }
+        setListUsers(d.data.result);
+        setMeta({
+            current: d.data.meta.current,
+            pageSize: d.data.meta.pageSize,
+            pages: d.data.meta.pages,
+            total: d.data.meta.total
+        });
+    };
+
     return (
         <div style={{
             margin: "0 20px"
@@ -177,6 +214,15 @@ const UsersTable = () => {
                 columns={columns}
                 dataSource={listUsers}
                 rowKey={"_id"}
+                pagination={{
+                    current: meta.current,
+                    pageSize: meta.pageSize,
+                    total: meta.total,
+                    showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`,
+                    onChange: (page: number, pageSize: number) => handleOnChange(page, pageSize),
+                    showSizeChanger: true
+                }}
+                loading={!listUsers || listUsers.length === 0 ? true : false}
             />
             <CreateUserModal
                 access_token={access_token}
